e0d0d6101df16ef4f74ea48aa504ce6896354988
ronaldobergara <bergaraheadbanger@gmail.com>
2023-04-25 21:15:20 -0300

    teste

--- galeria/models.py
+++ galeria/models.py
@@ -7,5 +7,5 @@ class Fotografia(models.Model):
     foto      = models.CharField(max_length=100, null=False, blank=False)
 
     def __str__(self):
-        return f"Fotografia [nome={self.nome}]"
+        return f"Fotografia [nome={self.nome}] "
         
