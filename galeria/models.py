5b13a004a711661670d9899716a945e0fed4c615
ronaldobergara <bergaraheadbanger@gmail.com>
2023-04-26 20:59:32 -0300

    Alterando template e configurando Admin
    Aprendemos como fazer alterações nos templates para acessar as referências do banco de dados utilizando {{}} e {%%};
    Criamos um superuser no terminal para permitir o acesso ao Django Admin;
    Incluímos a tabela Fotografia no Django Admin, possibilitando o acesso, deleção, inclusão e alteração de itens na tabela do banco de dados através de uma rota na web;
    Personalizamos o Django Admin para melhorar a usabilidade do administrador do nosso site.

--- galeria/models.py
+++ galeria/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 class Fotografia(models.Model):
+    OPCAO_CATEGORIA = [
+        ('NEBULOSA', 'Nebulosa'),
+        ('ESTRELA', 'Estrela'),
+        ('GALÁXIA', 'Galáxia'),
+        ('PLANETA', 'Planeta'),
+    ]
+
     nome      = models.CharField(max_length=100, null=False, blank=False)
     legenda   = models.CharField(max_length=100, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCAO_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     foto      = models.CharField(max_length=100, null=False, blank=False)
 
