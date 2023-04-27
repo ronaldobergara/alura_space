00a1bc4f6bfe72a417fb6f98972acd19e62da224
ronaldobergara <bergaraheadbanger@gmail.com>
2023-04-27 20:19:12 -0300

    Adicionado novos campos
    Adicionamos dois novos campos no model Fotografia. Um deles guarda informações de data e o outro se trata de um campo booleano;
    Através dos novos campos, adicionamos a funcionalidade de publicação e a funcionalidade de sempre mostrar as fotografias mais recentes na página principal.

--- galeria/admin.py
+++ galeria/admin.py
@@ -4,8 +4,11 @@ from galeria.models import Fotografia
 # Register your models here.
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ["id", "nome" ,"legenda"]
-    list_display_links = ("id", "nome" ,"legenda")
+    list_display = ["id", "data_fotografia","nome" ,"legenda", "publicada"]
+    list_display_links = ("id", "data_fotografia", "nome" ,"legenda")
     search_fields = ["nome"]
+    list_filter = ("categoria", )
+    list_per_page = 10
+    list_editable = ("publicada",)
 
 admin.site.register(Fotografia, ListandoFotografias)
