4e7c76c92a901d5627f8e74255baa5204cf3e330
ronaldobergara <bergaraheadbanger@gmail.com>
2023-05-16 21:49:23 -0300

    Campo usuario no model fotografia e validações
    Criamos um novo campo no model de Fotografia e o associamos à tabela de usuários presente no banco de dados interno do Django;
    Criamos novas validações nos dados vindos do formulário utilizando a boa prática do Clean;
    Inserimos mensagens de erro personalizadas para cada campo dos formulário que pudesse ter seu input incorreto.

--- galeria/admin.py
+++ galeria/admin.py
@@ -7,7 +7,7 @@ class ListandoFotografias(admin.ModelAdmin):
     list_display = ["id", "data_fotografia","nome" ,"legenda", "publicada"]
     list_display_links = ("id", "data_fotografia", "nome" ,"legenda")
     search_fields = ["nome"]
-    list_filter = ("categoria", )
+    list_filter = ("categoria", "usuario",)
     list_per_page = 10
     list_editable = ("publicada",)
 
--- usuarios/views.py
+++ usuarios/views.py
@@ -38,9 +38,9 @@ def cadastro(request):
         form = CadastroForms(request.POST)
 
         if form.is_valid():
-            if form['senha_1'].value() != form['senha_2'].value():
-                messages.error(request, "Senhas não são iguais")
-                return redirect('cadastro')
+            # if form['senha_1'].value() != form['senha_2'].value():
+            #     messages.error(request, "Senhas não são iguais")
+            #     return redirect('cadastro')
 
             nome  = form["nome_cadastro"].value()
             email = form["email"].value()
