c7f4216fce7ae9c6a0a63b4865782121f8949b4a
ronaldobergara <bergaraheadbanger@gmail.com>
2023-06-02 19:56:23 -0300

    Criando opção de cadastrar uma nova foto
    Criamos um formulário a partir de um model existente;
    Recebemos os dados do formulário preenchido e criamos um novo item na tabela do banco de dados usando o comando form.save();
    Criamos algumas novas views alterando o arquivo de URLs, views e diretório de templates.

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from apps.galeria.models import Fotografia
+from apps.galeria.forms import FotografiaForms
 
 from django.contrib import messages
 
@@ -29,3 +30,25 @@ def buscar(request):
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
     return render(request, "galeria/buscar.html", {"cards": fotografias})
+
+
+def nova_imagem(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado")
+        return redirect('login')    
+    
+    form = FotografiaForms
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Nova fotografia cadastrada!')
+            return redirect('index')
+        
+    return  render(request, 'galeria/nova_imagem.html', {'form': form})
+
+def editar_imagem(request):
+    pass
+
+def deletar_imagem(request):
+    pass
